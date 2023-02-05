316a1e46b2b26fa94403b844097986832ade78b2
Armin Hossein Zadeh <arminfrontend@gmail.com>
2023-02-05 21:08:44 +0330

    Added number state

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,14 +1,18 @@
+import { useState } from "react"
 import DecreaseButton from "./DecreaseButton"
 import IncreaseButton from "./IncreaseButton"
 
 const Counter = () => {
+
+    const [number , setNumber] = useState(0)
+
     return (
         <>
         
         <div className="card flex flex-col justify-center items-center space-y-8">
 
             <div className="flex justify-center items-center rounded-full text-6xl text-white font-bold
-             w-[120px] h-[120px] bg-[#2c2c2c]">0</div>
+             w-[120px] h-[120px] bg-[#2c2c2c]">{number}</div>
             <div className="flex space-x-20">
                 <IncreaseButton/>
                 <DecreaseButton/>
