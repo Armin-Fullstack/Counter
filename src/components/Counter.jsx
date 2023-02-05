88a4441a09d5249033fe6943f001d6c5b82a4cea
Armin Hossein Zadeh <arminfrontend@gmail.com>
2023-02-05 21:13:29 +0330

    Feat: Implemented the onIncreaseHandler

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -6,6 +6,10 @@ const Counter = () => {
 
     const [number , setNumber] = useState(0)
 
+    const onIncreaseHandler = () => {
+        setNumber(number + 1)
+    }
+
     return (
         <>
         
@@ -14,7 +18,7 @@ const Counter = () => {
             <div className="flex justify-center items-center rounded-full text-6xl text-white font-bold
              w-[120px] h-[120px] bg-[#2c2c2c]">{number}</div>
             <div className="flex space-x-20">
-                <IncreaseButton/>
+                <IncreaseButton onIncreaseHandler={onIncreaseHandler}/>
                 <DecreaseButton/>
             </div>
         </div>
--- src/components/IncreaseButton.jsx
+++ src/components/IncreaseButton.jsx
@@ -1,8 +1,8 @@
 
-const IncreaseButton = () => {
+const IncreaseButton = ({onIncreaseHandler}) => {
     return (
 
-       <button className="text-white bg-green-600
+       <button onClick={onIncreaseHandler} className="text-white bg-green-600
        text-5xl rounded-b-xl w-[50px] h-[100px] font-bold">+</button>
 
     )
